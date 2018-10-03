1c1b58d5f7b79e6c20af888115af121ee0b22d96
adore68 <adore68@naver.com>
2018-10-03 23:25:35 +0900

    계산기 클래스 만들기(day3) private으로 frontnumber, operator, backnumber 변수 public으로 계산식 등록과 결과 출력 메소드 main 함수에서 식 입력받아서 계산기 클래스 생성자에 parameter로 font, op, back 넘겨주기(이름상관x) 클래스의 메소드를 통해 계산 결과값 받아서 main 함수에서 출력

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,34 +1,61 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	float frontnumber, backnumber;
+class MyCaculator {
+private:
+	//멤버변수
+	double frontnumber;
 	char inputoperator;
+	double backnumber; 	
+public:
+	double result = 0;
+	//생성자 필요..? 생성자가 정확히 뭐지
 	
-	cout << "input : ";
-	cin >> frontnumber >> inputoperator >> backnumber;
+	//계산식 등록 메소드
+	double setNumber(double a, char c, double b) {
+		frontnumber = a;
+		inputoperator = c;
+		backnumber = b;
 	
-	switch (inputoperator) {
-	case '+':
-		cout << frontnumber + backnumber;
-		break;
-	case '-':
-		cout << frontnumber - backnumber;
-		break;
-	case '/':
-		cout << frontnumber / backnumber;
-		break;
-	case '*':
-		cout << frontnumber * backnumber;
-		break;
-	case '%':
-		if (backnumber != 0) {
-			cout << (int)frontnumber % (int)backnumber;
-			break;
+		if (inputoperator == '+') {
+			result = frontnumber + backnumber;
+			return result;
 		}
-		else if (backnumber == 0) {
-			cout << "cannot divide by 0!" << endl;
+		else if (inputoperator == '-') {
+			result = frontnumber - backnumber;
+			return result;
+		}
+		else if (inputoperator = '/'){
+			if (backnumber != '0') {
+				result = frontnumber / backnumber;
+				return result;
+			}
+			else if (backnumber == '0') {
+				cout <<  "cannot divide by 0!" << endl;
+			}
+		}
+		else if (inputoperator = '%') {
+			result = (int)frontnumber % (int)backnumber;
+			return result;
 		}
 	}
 	
+	// 결과출력 메소드
+	void Print() {
+		cout << "output : " << result << endl;
+	}
+	
+};
+
+int main() {
+	MyCaculator sample; // 객체생성
+	double a;
+	char c;
+	double b;
+	cout << "input : ";
+	//식 입력받기
+	cin >> a >> c >> b;
+	//계산기 클래스 생성자에 parameter로 front, op, back 넘겨줌(이름상관x)
+	sample.setNumber(a, c, b);
+	sample.Print();
 }
